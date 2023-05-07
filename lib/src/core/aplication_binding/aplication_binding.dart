a59ba13a7fad63bd436119b7fb5d54b300b4e9d6
gC4d <guilhermeacadorin@gmail.com>
2023-05-06 22:49:55 -0300

    providing a repositories

--- lib/src/core/aplication_binding/aplication_binding.dart
+++ lib/src/core/aplication_binding/aplication_binding.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:mob_storage_app/src/core/data/database/database.dart';
 import 'package:mob_storage_app/src/core/services/client/rest_client.dart';
 import 'package:provider/provider.dart';
 
+import '../repositories/storages/storage_repository.dart';
+import '../repositories/storages/storage_repository_impl.dart';
+import '../repositories/user/user_repository.dart';
+import '../repositories/user/user_repository_impl.dart';
+
 class AplicationBinding extends StatelessWidget {
   final Widget child;
-  const AplicationBinding({ Key? key, required this.child }) : super(key: key);
+  const AplicationBinding({Key? key, required this.child}) : super(key: key);
 
-   @override
-   Widget build(BuildContext context) {
-       return MultiProvider(
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
-        Provider(create: (context) => RestClient())
+        Provider(create: (context) => RestClient()),
+        Provider(create: (context) => AppDb()),
+        Provider<UserRepository>(
+          create: (context) => UserRepositoryImpl(
+            client: context.read(),
+          ),
+        ),
+        Provider<StorageRepository>(
+          create: (context) => StorageRepositoryImpl(
+            client: context.read(),
+            db: context.read(),
+            userRepository: context.read(),
+          ),
+        ),
       ],
       child: child,
     );
