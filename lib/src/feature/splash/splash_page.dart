6abdbc3b52406978b5556dbfbf261828c1e7defa
gC4d <guilhermeacadorin@gmail.com>
2023-06-11 20:08:54 -0300

    REFACTOR splash_page

--- lib/src/feature/splash/splash_page.dart
+++ lib/src/feature/splash/splash_page.dart
@@ -4,8 +4,6 @@ import 'package:mob_storage_app/src/core/ui/widgets/primary_button.dart';
 import 'package:mob_storage_app/src/core/ui/widgets/secundary_button.dart';
 import 'package:mob_storage_app/src/core/ui/widgets/title_widget.dart';
 
-import '../../core/ui/base_state/base_state.dart';
-
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
@@ -14,11 +12,10 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -26,19 +23,22 @@ class _SplashPageState extends State<SplashPage> {
             Container(
               height: 200,
               width: 200,
-              color: Theme.of(context).colorScheme.onSecondary,
-              child: Text('Logo'),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              child: const Center(child: Text('Logo')),
             ),
             SizedBox(
               height: context.percentHeight(.2),
             ),
             const TitleWidget(label: "Bem vindo ao"),
-            const TitleWidget(label: "MobileStorage"),
+            const TitleWidget(label: "MobStock"),
             SizedBox(
               height: context.percentHeight(.03),
             ),
             PrimaryButton(
-              label: "Login",
+              label: "Acessar",
               width: context.percentWidth(.7),
               onPressed: () {
                 Navigator.of(context).pushNamed('/auth');
