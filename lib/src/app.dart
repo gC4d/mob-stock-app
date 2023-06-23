33e0fe951fa5bfdf1f415e80e1d14e23ed965d55
Guilherme Cadorin <cadorin@pop-os.localdomain>
2023-06-23 19:05:41 -0300

    FEAT ROUTES: adding a routes method

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,14 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mob_storage_app/src/core/aplication_binding/aplication_binding.dart';
 import 'package:mob_storage_app/src/core/ui/routes/routes.dart';
 import 'package:mob_storage_app/src/core/ui/styles/themes.dart';
-import 'package:mob_storage_app/src/feature/auth/auth_router.dart';
-import 'package:mob_storage_app/src/feature/splash/splash_page.dart';
-import 'package:mob_storage_app/src/feature/validation/validation_page.dart';
-import 'package:mob_storage_app/src/feature/validation/validation_router.dart';
-import 'feature/group/group_router.dart';
-import 'feature/home/home_router.dart';
-import 'feature/signup/sign_up_router.dart';
-import 'feature/stock/stock_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -23,16 +15,7 @@ class App extends StatelessWidget {
         darkTheme: darkTheme,
         themeMode: ThemeMode.system,
         initialRoute: Routes.VALIDATION,
-        routes: {
-          Routes.SPLASH: (context) => const SplashPage(),
-          Routes.SIGN_UP : (context) => SignUpRouter.page,
-          Routes.AUTH:(context) => AuthRouter.page,
-          Routes.GROUP: (context) => GroupRouter.page, 
-          Routes.HOME: (context) => HomeRouter.page,
-          '/stock': (context) => StockRouter.page,
-          Routes.ADD_STOCK: (context) => StockRouter.addPage,
-          Routes.VALIDATION: (context) => ValidationRouter.page,
-        },
+        routes: Routes.routes,
       ),
     );
   }
