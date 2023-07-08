9739bd209e189e46cc078b1088c1b9302885a29d
Guilherme Cadorin <cadorin@pop-os.localdomain>
2023-07-07 22:41:23 -0300

    FEAT [groups controller]: implemented the method to get all of groups user can acess

--- lib/src/feature/group/group_controller.dart
+++ lib/src/feature/group/group_controller.dart
@@ -1,9 +1,28 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:mob_storage_app/src/core/repositories/group/group_repository.dart';
 import 'package:mob_storage_app/src/feature/group/group_state.dart';
 
+import '../../core/data/database/database.dart';
+
 class GroupController extends Cubit<GroupState> {
-  final GroupController _groupController;
+  final GroupRepository _groupController;
   GroupController(
     this._groupController,
   ) : super(const GroupState.initial());
+
+  Future<void> getGroups() async {
+    emit(state.copyWith(status: GroupStateStatus.loading));
+    List<GroupData> groups = [];
+    try {
+      groups = await _groupController.localGetAllGroups();
+      emit(state.copyWith(
+        groups: groups,
+        status: GroupStateStatus.success,
+      ));
+    } catch (e) {
+      log("Error in get user groups");
+    }
+  }
 }
