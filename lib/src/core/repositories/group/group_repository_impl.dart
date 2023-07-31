d6f00f64bf111a0e27c587ac0ab443af1885412e
Guilherme Cadorin <cadorin@pop-os.localdomain>
2023-07-30 23:05:59 -0300

    [FEAT] groups: implemented the method to create a new group in the server database

--- lib/src/core/repositories/group/group_repository_impl.dart
+++ lib/src/core/repositories/group/group_repository_impl.dart
@@ -1,18 +1,45 @@
+//import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:mob_storage_app/src/core/data/controller/data_controller.dart';
 import 'package:mob_storage_app/src/core/data/database/database.dart';
+import 'package:mob_storage_app/src/core/data/dto/group_dto.dart';
+import 'package:mob_storage_app/src/core/repositories/cryptography/cryptography_repository.dart';
+import 'package:mob_storage_app/src/core/services/client/rest_client.dart';
 
 import './group_repository.dart';
 
 class GroupRepositoryImpl implements GroupRepository {
   final DataController _database;
 
-  GroupRepositoryImpl(this._database);
+  final RestClient _client;
 
+  GroupRepositoryImpl(this._database, this._client);
 
   @override
-  Future<void> createGroup(GroupData stock) {
-    // TODO: implement createGroup
-    throw UnimplementedError();
+  Future<void> localCreateGroup(GroupCompanion group) async {
+    try {
+      await _database.createGroup(group);
+    } catch (e) {
+      log('error in GroupRepositoryImpl.localCreateGroup()');
+      log(e.toString());
+      log(StackTrace.current.toString());
+    }
+  }
+
+  @override
+  Future<List<GroupData>> localGetAllGroups() async {
+    List<GroupData> groups = [];
+    try {
+      groups = await _database.getAllGroups();
+      return groups;
+    } catch (e) {
+      log('error in GroupRepositoryImpl.localGetAllGroup()');
+      log(e.toString());
+      log(StackTrace.current.toString());
+    }
+    return groups;
   }
 
   @override
@@ -28,15 +55,67 @@ class GroupRepositoryImpl implements GroupRepository {
   }
 
   @override
-  Future<List<GroupData>> localFindAllStocks() {
-    // TODO: implement localFindAllStocks
-    throw UnimplementedError();
-  }
-
-  @override
   Future<void> localUpdateStorage(Map<String, dynamic> storage) {
     // TODO: implement localUpdateStorage
     throw UnimplementedError();
   }
 
+  @override
+  Future<void> syncCreateGroup(GroupDTO groupDTO) async {
+    try {
+      Response resCreateGroup =
+          await _client.auth().post('/group/create', data: {
+        'user_id': 1, // alterar depois
+        'description': groupDTO.description,
+        'key': groupDTO.key,
+        'password': groupDTO.password,
+      });
+
+      Response resCreateAccess =
+          await _client.auth().post('/group/add/access', data: {
+        'user_id': 1, // alterar depois]
+        'group_id': resCreateGroup.data['id'],
+        'access': 1
+      });
+
+      Response resCreatePermissions =
+          await _client.auth().post('/group/create/permission', data: {
+            'user_id': 1, // alterar depois
+            'group_id': resCreateGroup.data['id'],
+            'p_add_remove': 1,
+            'p_create': 1,
+            'p_delete': 1,
+            'is_adm': 1,
+          });
+      log(resCreateGroup.data.toString());
+    } catch (e) {
+      log('error in GroupRepositoryImpl.syncCreateGroup()');
+      log(e.toString());
+      log(StackTrace.current.toString());
+    }
+  }
+
+  @override
+  Future<void> syncDeleteAllStocks() {
+    // TODO: implement syncDeleteAllStocks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> syncDeleteStock(int id) {
+    // TODO: implement syncDeleteStock
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<GroupData>> syncGetAllGroups() {
+    // TODO: implement syncGetAllGroups
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> syncUpdateStorage(Map<String, dynamic> storage) {
+    // TODO: implement syncUpdateStorage
+    throw UnimplementedError();
+  }
 }
--- lib/src/feature/group/group_controller.dart
+++ lib/src/feature/group/group_controller.dart
@@ -1,28 +1,49 @@
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:mob_storage_app/src/core/data/dto/group_dto.dart';
 import 'package:mob_storage_app/src/core/repositories/group/group_repository.dart';
 import 'package:mob_storage_app/src/feature/group/group_state.dart';
 
 import '../../core/data/database/database.dart';
+import '../../core/repositories/cryptography/cryptography_repository.dart';
 
 class GroupController extends Cubit<GroupState> {
-  final GroupRepository _groupController;
-  GroupController(
-    this._groupController,
-  ) : super(const GroupState.initial());
+  final GroupRepository groupRepository;
+  final CryptographyRepository crypto;
+  late var groupDTO = GroupDTO(id: 0, description: '', key: '', password: '');
+
+  GroupController({
+    required this.groupRepository,
+    required this.crypto,
+  }) : super(const GroupState.initial());
 
   Future<void> getGroups() async {
     emit(state.copyWith(status: GroupStateStatus.loading));
     List<GroupData> groups = [];
     try {
-      groups = await _groupController.localGetAllGroups();
+      groups = await groupRepository.localGetAllGroups();
       emit(state.copyWith(
         groups: groups,
         status: GroupStateStatus.success,
       ));
     } catch (e) {
       log("Error in get user groups");
+    }
+  }
+
+  Future<void> createGroups() async {
+    emit(state.copyWith(status: GroupStateStatus.loading));
+    try {
+      if (groupDTO.description != '' && groupDTO.password != '') {
+        groupDTO.key = await crypto.keyGenerator(groupDTO.description,
+            groupDTO.password, DateTime.now(), "guilhermeacadorin@gmail.com");
+        log(groupDTO.key);
+        await groupRepository.syncCreateGroup(groupDTO);
+      }
+      emit(state.copyWith(status: GroupStateStatus.success));
+    } catch (e) {
+      log('error in groupController.createGroups()');
     }
   }
 }
